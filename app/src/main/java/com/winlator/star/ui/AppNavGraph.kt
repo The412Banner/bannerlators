d1d5bcfdf2e8924aed922e69bfa1b838c08e7586
AGBOOLA ISRAEL OLUWAGBOGO <israelagboola53@gmail.com>
2026-05-27 04:28:24 +0100

    feat: add LsfgSettings route to AppNavGraph.kt

--- app/src/main/java/com/winlator/star/ui/AppNavGraph.kt
+++ app/src/main/java/com/winlator/star/ui/AppNavGraph.kt
@@ -20,6 +20,7 @@ import com.winlator.star.ui.screens.FileManagerScreen
 import com.winlator.star.ui.screens.FragmentScreen
 import com.winlator.star.ui.screens.SavesScreen
 import com.winlator.star.ui.screens.ShortcutsScreen
+import com.winlator.star.ui.screens.LsfgSettingsScreen
 
 @Composable
 fun AppNavGraph(
@@ -94,5 +95,8 @@ fun AppNavGraph(
             SavesScreen()
         }
 
+        composable(Screen.LsfgSettings.route) {
+            LsfgSettingsScreen()
+        }
     }
 }
